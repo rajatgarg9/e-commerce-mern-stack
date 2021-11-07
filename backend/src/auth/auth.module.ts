d2f09282245998038674d5577e62f378fb7e483c
Rajat Garg <gargrajat961@gmail.com>
2021-11-07 18:39:30 +0530

    mongo setup

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 
+import { UserSchema } from './schema/user.schema';
+
 @Module({
-  imports: [],
+  imports: [MongooseModule.forFeature([{ name: 'users', schema: UserSchema }])],
   controllers: [AuthController],
   providers: [AuthService],
 })
--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -1,8 +1,20 @@
 import { Injectable } from '@nestjs/common';
 
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+
+import { LoginDto, SignUpDto } from './dto';
+
+import { UserDocument, Test } from './schema/user.schema';
+
 @Injectable()
 export class AuthService {
+  constructor(@InjectModel('users') private userModel: Model<Test>) {}
+
   signUp(): any {
+    const createdCat = new this.userModel();
+    return createdCat.save();
+    // this.userModel.
     return 'Hello World1!';
   }
 }

1ee71d5efe37dc147b12b7e16ca8e98048f23c3c
Rajat Garg <gargrajat961@gmail.com>
2021-12-18 23:50:29 +0530

    eslint and prettier setup

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
+
+const ESLintPlugin = require('eslint-webpack-plugin');
+
 module.exports = {
   reactStrictMode: true,
-}
+  webpack: (config, options) => {
+    config.plugins.push(new ESLintPlugin({
+      extensions: ['ts', 'tsx'],
+      failOnWarning:true
+    }));
+
+    return config;
+  },
+};

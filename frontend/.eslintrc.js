59e96a03dc739a98233f14f07b9bfd9d28362ec3
Rajat Garg <gargrajat961@gmail.com>
2021-12-19 16:48:00 +0530

    eslint and stylelint config

---
+++ frontend/.eslintrc.js
@@ -0,0 +1,35 @@
+module.exports = {
+  env: {
+    browser: true,
+  },
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint", "prettier"],
+  extends: [
+    "eslint:recommended",
+    "airbnb",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:@typescript-eslint/recommended-requiring-type-checking",
+    "next/core-web-vitals",
+    "plugin:prettier/recommended",
+  ],
+  parserOptions: {
+    sourceType: "module",
+    tsconfigRootDir: __dirname,
+    project: "tsconfig.json",
+  },
+  ignorePatterns: [".eslintrc.js", "next.config.js"],
+  settings: {
+    next: {
+      rootDir: "./",
+    },
+  },
+  rules: {
+    "prettier/prettier": "error",
+    "arrow-body-style": "off", // for prettier plugin
+    "prefer-arrow-callback": "off", // for prettier plugin
+    "react/function-component-definition": "off",
+    "react/jsx-props-no-spreading": "off",
+    "react/jsx-filename-extension": [2, { extensions: [".tsx"] }],
+    "no-unused-vars": 2,
+  },
+};
--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,14 +1,18 @@
 /** @type {import('next').NextConfig} */
 
-const ESLintPlugin = require('eslint-webpack-plugin');
+const ESLintPlugin = require("eslint-webpack-plugin");
+const StylelintPlugin = require("stylelint-webpack-plugin");
 
 module.exports = {
   reactStrictMode: true,
   webpack: (config, options) => {
-    config.plugins.push(new ESLintPlugin({
-      extensions: ['ts', 'tsx'],
-      failOnWarning:true
-    }));
+    config.plugins.push(
+      new ESLintPlugin({
+        extensions: ["ts", "tsx"],
+        failOnWarning: true,
+      }),
+      new StylelintPlugin({ failOnWarning: true })
+    );
 
     return config;
   },

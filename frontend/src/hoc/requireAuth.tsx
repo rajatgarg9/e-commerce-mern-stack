230633827751205ccb894bccd69a2f6da8209afc
Rajat Garg <gargrajat961@gmail.com>
2022-03-20 18:30:39 +0530

    auth hoc partial solution

--- frontend/src/hoc/requireAuth.tsx
+++ frontend/src/hoc/requireAuth.tsx
@@ -1,13 +1,18 @@
-import React, { ComponentType } from "react";
+/* eslint-disable @typescript-eslint/no-unsafe-return */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+import React from "react";
 import { useSelector } from "react-redux";
+import { NextPageContext } from "next";
 
 import PageLoader from "@src/non-route-pages/page-loader/page-loader";
 import AuthenticationPage from "@src/non-route-pages/authentication-page/authentication-page";
 
 import { IRootReducerState } from "@action-reducers/root.reducer";
 
-function requireAuth<PropTypes>(WrappedComponent: ComponentType) {
-  return function ChildComponent(props: PropTypes) {
+function requireAuth<PropTypes>(WrappedComponent: any) {
+  function ChildComponent(props: PropTypes) {
     const accessToken = useSelector(
       (state: IRootReducerState) => state.auth.accessToken,
     );
@@ -24,7 +29,18 @@ function requireAuth<PropTypes>(WrappedComponent: ComponentType) {
     }
 
     return <WrappedComponent {...props} />;
+  }
+
+  ChildComponent.getInitialProps = async (ctx: NextPageContext) => {
+    const pageProps =
+      WrappedComponent?.getInitialProps &&
+      (await WrappedComponent.getInitialProps(ctx));
+    // Return props.
+
+    return { ...pageProps };
   };
+
+  return ChildComponent;
 }
 
 export default requireAuth;
